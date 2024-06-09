2d673a5c2fa1d3edc47f003112b41200a73d2322
dheasalsaa <dheasalsa15@gmail.com>
2024-06-09 20:23:50 +0700

    update

--- main.py
+++ main.py
@@ -107,6 +107,40 @@ def tambah_penyewaan(penyewaan: Penyewaan):
 def get_penyewaan():
     return data_penyewaan
 
+def get_penyewaan_index(id_penyewaan):
+    for index, penyewaan in enumerate(data_penyewaan):
+        if penyewaan['id_penyewaan'] == id_penyewaan:
+            return index
+    return None
+
+# Endpoint untuk detail get id
+@app.get("/penyewaan/{id_penyewaan}", response_model=Optional[Penyewaan])
+def get_penyewaan_by_id(id_penyewaan: str):
+    for penyewaan in data_penyewaan:
+        if penyewaan['id_penyewaan'] == id_penyewaan:
+            return Penyewaan(**penyewaan)
+    return None
+
+# Endpoint untuk memperbarui data pengembalian dengan hanya memasukkan id_pengembalian
+@app.put("/penyewaan/{id_penyewaan}")
+def update_penyewaan_by_id(id_penyewaan: str, penyewaan_baru: Penyewaan):
+    index = get_penyewaan_index(id_penyewaan)
+    if index is not None:
+        data_penyewaan[index] = penyewaan_baru.dict()
+        return {"message": "Data penyewaan berhasil diperbarui."}
+    else:
+        raise HTTPException(status_code=404, detail="Data penyewaan tidak ditemukan.")
+
+# Endpoint untuk menghapus data penyewaan
+@app.delete("/penyewaan/{id_penyewaan}")
+def delete_penyewaan(id_penyewaan: str):
+    index = get_penyewaan_index(id_penyewaan)
+    if index is not None:
+        del data_penyewaan[index]
+        return {"message": "Data penyewaan berhasil dihapus."}
+    else:
+        raise HTTPException(status_code=404, detail="Data penyewaan tidak ditemukan.")
+
 # Model untuk Data Pembayaran
 class Pembayaran(BaseModel):
     id_pembayaran: str
@@ -132,6 +166,7 @@ def tambah_pembayaran(pembayaran: Pembayaran):
 @app.get("/pembayaran", response_model=List[Pembayaran])
 def get_pembayaran():
     return data_pembayaran
+
 
 class Pengembalian(BaseModel):
     id_pengembalian: str
@@ -277,3 +312,237 @@ class Penduduk(BaseModel):
 def get_penduduk():
     data_penduduk = get_data_penduduk_from_web()
     return data_penduduk
+
+
+
+
+
+
+
+def get_data_tourGuide_from_web():
+    url = "https://tour-guide-ks4n.onrender.com/tourguide"  # Ganti dengan URL yang sebenarnya
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data TOUR GUIDE dari web hosting.")
+
+# Model untuk Data Tour Guide
+class TourGuide(BaseModel):
+    id_guider: str
+    nama_guider: str
+    no_hp: str
+
+# Endpoint untuk mendapatkan data Tour Guide
+@app.get("/tourGuide", response_model=List[TourGuide])
+def get_tourGuide():
+    data_tourGuide = get_data_tourGuide_from_web()
+    return data_tourGuide
+
+def get_tourGuide_index(id_guider):
+    data_tourGuide = get_data_tourGuide_from_web()
+    for index, tourGuide in enumerate(data_tourGuide):
+        if tourGuide['id_guider'] == id_guider:
+            return index
+    return None
+
+@app.get("/tourGuide/{id_guider}", response_model=Optional[TourGuide])
+def get_tourGuide_by_id(id_guider: str):
+    data_tourGuide = get_data_tourGuide_from_web()
+    for tourGuide in data_tourGuide:
+        if tourGuide['id_guider'] == id_guider:
+            return TourGuide(**tourGuide)
+    return None
+
+
+
+
+
+# Fungsi untuk mengambil data asuransi dari web hosting lain
+def get_data_asuransi_from_web():
+    url = "https://eai-fastapi.onrender.com/asuransi"  # Ganti dengan URL yang sebenarnya
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data ASURANSI dari web hosting.")
+
+# Model untuk Data Asuransi
+class Asuransi(BaseModel):
+    id_asuransi: str
+
+def get_asuransi_index(id_asuransi):
+    data_asuransi = get_data_asuransi_from_web()
+    for index, asuransi in enumerate(data_asuransi):
+        if asuransi['id_asuransi'] == id_asuransi:
+            return index
+    return None
+
+@app.get("/asuransi", response_model=List[Asuransi])
+def get_asuransi():
+    data_asuransi = get_data_asuransi_from_web()
+    return data_asuransi
+
+@app.get("/asuransi/{id_asuransi}", response_model=Optional[Asuransi])
+def get_asuransi_by_id(id_asuransi: str):
+    data_asuransi = get_data_asuransi_from_web()
+    for asuransi in data_asuransi:
+        if asuransi['id_asuransi'] == id_asuransi:
+            return Asuransi(**asuransi)
+    return None
+
+
+
+
+# Fungsi untuk mengambil data pajak dari web hosting lain
+def get_data_government_from_web():
+    url = "https://api-government.onrender.com/penduduk"  # Ganti dengan URL yang sebenarnya
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data GOVERNMENT dari web hosting.")
+
+# Model untuk Data Pajak
+class Government(BaseModel):
+    nik: int
+    nama: str
+
+# Endpoint untuk mendapatkan data pajak
+@app.get("/government", response_model=List[Government])
+def get_government():
+    data_government = get_data_government_from_web()
+    return data_government
+
+def get_government_index(id_government):
+    data_government = get_data_government_from_web()
+    for index, government in enumerate(data_government):
+        if government['id_government'] == id_government:
+            return index
+    return None
+
+@app.get("/government/{id_government}", response_model=Optional[Government])
+def get_government_by_id(id_government: str):
+    data_government = get_data_government_from_web()
+    for government in data_government:
+        if government['id_government'] == id_government:
+            return Government(**government)
+    return None
+
+
+
+
+
+
+# Fungsi untuk mengambil data objekWisata dari web hosting lain
+def get_data_objekWisata_from_web():
+    url = "https://pajakobjekwisata.onrender.com/wisata"  # Ganti dengan URL yang sebenarnya
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data OBJEK WISATA dari web hosting.")
+
+# Model untuk Data ObjekWisata
+class ObjekWisata(BaseModel):
+    id_wisata: str
+
+# Endpoint untuk mendapatkan data objekWisata
+@app.get("/objekWisata", response_model=List[ObjekWisata])
+def get_objekWisata():
+    data_objekWisata = get_data_objekWisata_from_web()
+    return data_objekWisata
+
+def get_objekWisata_index(id_wisata):
+    data_objekWisata = get_data_objekWisata_from_web()
+    for index, objekWisata in enumerate(data_objekWisata):
+        if objekWisata['id_wisata'] == id_wisata:
+            return index
+    return None
+
+@app.get("/objekWisata/{id_wisata}", response_model=Optional[ObjekWisata])
+def get_objekWisata_by_id(id_wisata: str):
+    data_objekWisata = get_data_objekWisata_from_web()
+    for objekWisata in data_objekWisata:
+        if objekWisata['id_wisata'] == id_wisata:
+            return ObjekWisata(**objekWisata)
+    return None
+
+
+
+
+# Fungsi untuk mengambil data bank dari web hosting lain
+def get_data_bank_from_web():
+    url = "https://undelaying-semaphor.000webhostapp.com/kartu-kredit"  # Ganti dengan URL yang sebenarnya
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data BANK dari web hosting.")
+
+# Model untuk Data Bank
+class Bank(BaseModel):
+    id: int
+    
+
+def get_bank_index(id):
+    data_bank = get_data_bank_from_web()
+    for index, bank in enumerate(data_bank):
+        if bank['id'] == id:
+            return index
+    return None
+
+@app.get("/bank", response_model=List[Bank])
+def get_bank():
+    data_bank = get_data_bank_from_web()
+    return data_bank
+
+@app.get("/bank/{id}", response_model=Optional[Bank])
+def get_bank_by_id(id: str):
+    data_bank = get_data_bank_from_web()
+    for bank in data_bank:
+        if bank['id'] == id:
+            return Bank(**bank)
+    return None
+
+
+
+
+
+def combine_mobil_asuransi():
+    mobil_data = get_mobil()
+    asuransi_data = get_asuransi()
+    combined_data = [
+        {
+            "id_mobil": mobil['id_mobil'],
+            "merek": mobil['merek'],
+            "model": mobil['model'],
+            "tipe_mobil": mobil['tipe_mobil'],
+            "tahun_pembuatan": mobil['tahun_pembuatan'],
+            "warna": mobil['warna'],
+            "nomor_polisi": mobil['nomor_polisi'],
+            "harga_sewa_per_hari": mobil['harga_sewa_per_hari'],
+            "status_ketersediaan": mobil['status_ketersediaan'],
+            "asuransi": asuransi
+        }
+        for mobil in mobil_data
+        for asuransi in asuransi_data
+    ]
+    return combined_data
+
+class MobilAsuransi(BaseModel):
+    id_mobil: str
+    merek: str
+    model: str
+    tipe_mobil: str
+    tahun_pembuatan: int
+    warna: str
+    nomor_polisi: str
+    harga_sewa_per_hari: int
+    status_ketersediaan: str
+    asuransi: Asuransi
+
+@app.get("/mobilAsuransi", response_model=List[MobilAsuransi])
+def get_combined_data():
+    combined_data = combine_mobil_asuransi()
+    return combined_data
