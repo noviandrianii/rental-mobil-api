71ecec638e86c69fddf50a83e6c9c53277d88f50
dheasalsaa <dheasalsa15@gmail.com>
2024-06-12 20:40:25 +0700

    coding terbaru

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\HP\OneDrive\Desktop\TEL-U SI '45\TERM 6\INTEGRASI APLIKASI ENTERPRISE\TUBES\rental-mobil-api\venv
+set VIRTUAL_ENV=C:\Users\HP\OneDrive\Desktop\TEL-U SI '45\TERM 6\INTEGRASI APLIKASI ENTERPRISE\rental-mobil-api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
